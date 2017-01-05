0ede1c012c96af3fbda5dbfba40fb24b265cddc2
KaiAmelung <kai.g.amelung@gmail.com>
2017-01-05 10:15:49 -0500

    stuff

--- rooms/index.js
+++ rooms/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var socketio = require('socket.io');
 var path = require('path');
 var admin = require('firebase-admin');
+var ref = admin.database().ref('/')
 admin.initializeApp({
   credential: admin.credential.cert("competitive-rps-firebase-adminsdk-aqmiv-272672c409.json"),
   databaseURL: "https://competitive-rps.firebaseio.com"
@@ -20,8 +21,25 @@ server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
 rooms = []
 
 io.sockets.on('connection', function(socket) {
+	socket.on('signup', function(creds){
+		var auth = admin.auth()
+		auth.createUserWithEmailAndPassword(creds.email, creds.password).then(function(user){
+			ref.child('users/'+user.uid).set({
+				email: creds.email,
+				elo: 1000
+			})
+			socket.emit("signUpSuccess", user.uid)
+		}, function(err){
+			socket.emit("errorInSignup", err.message)
+		});
+	})
 	socket.on('login', function(creds){
+		var auth = admin.auth()
+		auth.signInWithEmailAndPassword().then(function(user){
 
+		}, function(err){
+			
+		})
 	})
 	socket.on('join', function(user) {
         socket.join(room);
