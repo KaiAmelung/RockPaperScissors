3281306443981cf862ee08a46cdcc029ebaac0b8
KaiAmelung <kai.g.amelung@gmail.com>
2017-01-05 09:19:20 -0500

    socket update

--- rooms/index.js
+++ rooms/index.js
@@ -1,13 +1,24 @@
-var port = process.env.PORT || 8080;
-var io = require('socket.io')();
-io.attach(port);
-var games = [];
-io.on('connection', function(socket){
-	if(games.length==0) {
-		var room = Math.floor(Math.random(9000))+1000;
-		
-	}
-	else {
+var http = require('http');
+var express = require('express');
+var socketio = require('socket.io');
+var path = require('path');
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+router.use(express.static(path.resolve(__dirname, 'client')));
 
-	}
+server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
 });
+
+rooms = []
+
+io.sockets.on('connection', function(socket) {
+	socket.on('login', function(creds){
+
+	})
+	socket.on('join', function(user) {
+        socket.join(room);
+    });
+}
